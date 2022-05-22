173d39f9f28bc7eae7e9e6f47f91efb16a5697b0
Rafael masselli <89049153+rafaelmasselli@users.noreply.github.com>
2022-05-22 16:09:08 -0300

    Readme.md

--- README.md
+++ README.md
@@ -1,15 +1,51 @@
-<h1>Projeto pokedex, Modulo 2</h1>
+<h1 align="center" >⚡ Projeto Pokedex ⚡</h1>
+
+<div align="center">
+<img width="100px" src="https://img.shields.io/badge/lisence-MTI-green.svg">
+
+<br>
+<img width="120" src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E">
+<img width="95" src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white">
+<img width="70" src="  https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white">
+</div>
+
+<br>
+
+> Link do projeto : <https://pokedex-projec-996.herokuapp.com>
+
+<br>
+
+## Pre-requisitos 📝
+
+### Para iniciar o projeto você ira precisa do <a href="https://nodejs.org/en/">Node js</a> e de uma ide recomendo o <a href="https://visualstudio.microsoft.com/pt-br/downloads/">Visual studio</a> e do <a href="https://git-scm.com/downloads">git</a> para clonar o projeto
+
+<br>
+
+## Descrição
+
+### Uma pokedex que permite o usuario Criar e renderizar as informacoes criadas utilizando o json
 
 ![4744c54ee8019c0396f856deba6e96dc](https://user-images.githubusercontent.com/89049153/138615474-6c16db62-7a6e-4a1a-957f-d409707d8dbe.gif)
 
-<li>Uma pokedex que permite o usuario Criar e renderizar as informacoes criadas utilizando o json</li>
+>> Introdução a Node js, Express, Nodemon, Ejs e Heroku
 
-<li>Introducao a node js, express, nodemon, ejs e Heroku</li>
+<br>
 
-<li>Depedencias instaladas</li>
+## Iniciando o projeto 🎲
 
-<ul>npm install express</ul>
- <ul>npm install ejs</ul>
-<ul>npm install nodemon</ul>
+```
+// Clonando o projeto
+$ git clone https://github.com/rafaelmasselli/BF-Pokedex-Ejs-NodeJs
 
-<li> Site pra acessar o projeto: https://pokedex-projec-996.herokuapp.com</li>
+// Na pasta do projeto com uma ide ou terminal instale o projeto 
+$ npm install
+
+// Iniciando o projeto como Usuário 
+$ npm start 
+
+// Iniciando o projeto como Dev 
+$ npm start dev
+
+```
+
+
--- index.js
+++ index.js
@@ -2,42 +2,49 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000; // set port usando a var de ambiante ou a 3000
 const path = require("path");
+
 const Pokedex = [
-  {
-    Numero:252,
-    Nome:"Treecko",
-    Tipo:"Planta",
-    Image:"../IMG/planta.png",
-    Descricao:"Um pokemon que usa sua calda como metodo de ataque",
-    Altura:"0,5 ",
-    Peso:"5 ",
-    Categoria:"wood gecko",
-    Habilidade:"Esse pokemon tem garras que o faclita em escalas de paredes verticais"
-},
-{
-    Numero:255,
-    Nome:"Torchic",
-    Tipo:"Fogo",
-    Image:"../IMG/Fogo.png",
-    Descricao:"Esse pokemon respira fogo a mais de 1.800 graus fahreheit incluindo bolas de fogo que deixam o inimigo queimado",
-    Altura:"0,4 ",
-    Peso:"2.5 ",
-    Categoria:"chick",
-    Habilidade:"Torchic gruda em seu trainer, seguindo atras com pasos instaveis"
-},
-{  
-    Numero:"258",
-    Nome:"Mudkip",
-    Tipo:"Agua",
-    Image:"../IMG/Agua.png",
-    Descricao:"Esse pokemon pode determinar o que esta acontecendo ao seu redor sem usar os olhos",
-    Altura:"0.4 ",
-    Peso:"7.6 ",
-    Categoria:"Torrent",
-    Habilidade:"A barbatana na cabeca de mudkip atua como um rada altamente sensivel."
-}
-]
-let = message = ""
+ {
+  Numero: 252,
+  Nome: "Treecko",
+  Tipo: "Planta",
+  Image: "../IMG/planta.png",
+  Descricao: "Um pokemon que usa sua calda como metodo de ataque",
+  Altura: "0,5 ",
+  Peso: "5 ",
+  Categoria: "wood gecko",
+  Habilidade:
+   "Esse pokemon tem garras que o faclita em escalas de paredes verticais",
+ },
+ {
+  Numero: 255,
+  Nome: "Torchic",
+  Tipo: "Fogo",
+  Image: "../IMG/Fogo.png",
+  Descricao:
+   "Esse pokemon respira fogo a mais de 1.800 graus fahreheit incluindo bolas de fogo que deixam o inimigo queimado",
+  Altura: "0,4 ",
+  Peso: "2.5 ",
+  Categoria: "chick",
+  Habilidade:
+   "Torchic gruda em seu trainer, seguindo atras com pasos instaveis",
+ },
+ {
+  Numero: "258",
+  Nome: "Mudkip",
+  Tipo: "Agua",
+  Image: "../IMG/Agua.png",
+  Descricao:
+   "Esse pokemon pode determinar o que esta acontecendo ao seu redor sem usar os olhos",
+  Altura: "0.4 ",
+  Peso: "7.6 ",
+  Categoria: "Torrent",
+  Habilidade:
+   "A barbatana na cabeca de mudkip atua como um rada altamente sensivel.",
+ },
+];
+
+let = message = "";
 
 //rota de arquivos
 
@@ -45,74 +52,79 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
-
 //menu principal com mensagem 5 S
 
 app.get("/", (req, res) => {
-  setTimeout(() => {
-    message = "";
-  }, 5000);
- res.render("index" , {Pokedex, message});
-})
+ setTimeout(() => {
+  message = "";
+ }, 5000);
+ res.render("index", { Pokedex, message });
+});
 
 // Cadastro
 
 app.get("/cadastro", (req, res) => {
-  res.render("cadastro");
+ res.render("cadastro");
 });
 
 //Detalhes
 
- app.get("/detalhes/:id", (req, res) => {
-    const id = req.params.id;
-    const pokemon = Pokedex[id];
-    res.render("detalhes",{pokemon,Pokedex});
-  });
-
-
+app.get("/detalhes/:id", (req, res) => {
+ const id = req.params.id;
+ const pokemon = Pokedex[id];
+ res.render("detalhes", { pokemon, Pokedex });
+});
 
 app.post("/New", (req, res) => {
-  const {Numero,Nome,Tipo,Image,Descricao,Altura,Peso,Categoria,Habilidade} = req.body;
-  if (!Numero){
-  res.redirect("/cadastro", {message:"tabela numero esta vazia"})
+ const {
+  Numero,
+  Nome,
+  Tipo,
+  Image,
+  Descricao,
+  Altura,
+  Peso,
+  Categoria,
+  Habilidade,
+ } = req.body;
+ if (!Numero) {
+  res.redirect("/cadastro", { message: "tabela numero esta vazia" });
+ } else if (!Nome) {
+  res.redirect("/cadastro", { message: "tabela nome esta vazia" });
+ } else if (!Tipo) {
+  res.redirect("/cadastro", { message: "tabela tipo esta vazia" });
+ } else if (!Image) {
+  res.redirect("/cadastro", { message: "tabela Imagem esta vazia" });
+ } else if (!Descricao) {
+  res.redirect("/cadastro", { message: "tabela Descricao esta vazia" });
+ } else if (!Altura) {
+  res.redirect("/cadastro", { message: "tabela Altura esta vazia" });
+ } else if (!Peso) {
+  res.redirect("/cadastro", { message: "tabela Peso esta vazia" });
+ } else if (!Categoria) {
+  res.redirect("/cadastro", { message: "tabela Categoria esta vazia" });
+ } else if (!Habilidade) {
+  res.redirect("/cadastro", { message: "tabela Habilidade esta vazia" });
+ }
 
-  }else if (!Nome){
-  res.redirect("/cadastro", {message:"tabela nome esta vazia"})
+ const novo = {
+  Numero: Numero,
+  Nome: Nome,
+  Tipo: Tipo,
+  Image: Image,
+  Descricao: Descricao,
+  Altura: Altura,
+  Peso: Peso,
+  Categoria: Categoria,
+  Habilidade: Habilidade,
+ };
 
-  }else if (!Tipo){
-  res.redirect("/cadastro", {message:"tabela tipo esta vazia"})
+ Pokedex.push(novo);
 
-  }else if (!Image){
-  res.redirect("/cadastro", {message:"tabela Imagem esta vazia"})
-
-  }else if (!Descricao){
-  res.redirect("/cadastro", {message:"tabela Descricao esta vazia"})
-
-  }else if (!Altura){
-  res.redirect("/cadastro", {message:"tabela Altura esta vazia"})
-
-  }else if (!Peso){
-  res.redirect("/cadastro", {message:"tabela Peso esta vazia"})
-
-  }else if (!Categoria){
-  res.redirect("/cadastro", {message:"tabela Categoria esta vazia"})
-
-  }else if (!Habilidade){
-  res.redirect("/cadastro", {message:"tabela Habilidade esta vazia"})
-  }
-
-
-  const novo = {Numero:Numero, Nome:Nome, Tipo:Tipo, Image:Image, Descricao:Descricao, Altura:Altura, Peso:Peso, Categoria:Categoria, Habilidade:Habilidade}
-  
-
-Pokedex.push(novo)
-
-message = `O pokemon ${Nome} foi adicionado`
-  res.redirect("/")
-})
-
-
+ message = `O pokemon ${Nome} foi adicionado`;
+ res.redirect("/");
+});
 
 app.listen(port, () =>
-  console.log(`Servidor rodando em http://localhost:${port}`)
-);  
+ console.log(`Servidor rodando em http://localhost:${port}`)
+);
